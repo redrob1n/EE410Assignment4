9b1cb38ba50ced9d097938eb45b64db5acf0f9a3
cdf0013 <cdf0013@uah.edu>
2018-10-23 21:07:25 -0500

    added feelforce

---
+++ Assignment4/feelforce.ino
@@ -0,0 +1,24 @@
+void Feelforce()
+{
+  float forceI[8];
+  float forceX = 0;
+  float forceY = 0;
+  float force, squares; 
+  double vectorAngle, angle;
+  int k;
+  
+  k = 20;
+  angle = 0;
+  
+  for(int i = 0; i < 8; i++;){
+    forceI[i] = k/(sonarRead[i]);
+    forceX =+ (forceI[i]*cos(angle);
+    forceY =+ (forceI[i]*sin(angle);
+    angle =+ PI/4;
+  }
+
+  squares = (forceX*forceX)+(forceY*forceY)
+  force = sqrt(squares);
+  vectorAngle = atan2(-forceY, -forceX);
+}
+
